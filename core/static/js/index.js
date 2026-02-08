89b481fa46df9808b34b76dd26e72c5e84c31d50
Italo-Dev1 <ItaloAraujoDDeus@hotmail.com>
2026-02-07 23:19:39 -0300

    Update index.js

--- core/static/js/index.js
+++ core/static/js/index.js
@@ -1,12 +1,9 @@
-// DEIXA OS MENUS COM ASPECTO SELECIONADO E DESMARCA O ANTERIOR
 const buttons = document.querySelectorAll('.container-options button');
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        // remove active de todos
         buttons.forEach(btn => btn.classList.remove('active'));
 
-        // adiciona active no clicado
         button.classList.add('active');
     });
 });
