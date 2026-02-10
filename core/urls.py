69fe08a2ee51c57407ede48919ba6cea7e0819dc
Italo-Dev1 <ItaloAraujoDDeus@hotmail.com>
2026-02-10 20:38:26 -0300

    fix: remove produtos route

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("produtos/", views.produtos, name="produtos"),
     path("exportar/", views.exportar_csv, name="exportar_csv"),
 ]
