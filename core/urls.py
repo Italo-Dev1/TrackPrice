5acfde910fbdc4e8081aaca4fb045092a0b00095
Italo-Dev1 <ItaloAraujoDDeus@hotmail.com>
2026-02-10 20:07:48 -0300

    fix settings for render deploy

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import dashboard, produtos, exportar_csv
+from . import views
 
 urlpatterns = [
-    path("", dashboard, name="dashboard"),
-    path("produtos/", produtos, name="produtos"),
-    path("relatorios/csv/", exportar_csv, name="exportar_csv"),
+    path("", views.index, name="index"),
+    path("produtos/", views.produtos, name="produtos"),
+    path("exportar/", views.exportar_csv, name="exportar_csv"),
 ]
